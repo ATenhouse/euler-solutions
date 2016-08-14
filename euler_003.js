1e38d618aa2a8ff021a97e58bb254cd237887b77
chistery <Adam Tenhouse, Esquire>
2016-08-14 00:36:49 -0500

    Added initial four solutions

---
+++ euler_003.js
@@ -0,0 +1,31 @@
+// Problem 3
+// =========
+// 
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// 
+// What is the largest prime factor of the number 600851475143?
+
+function run(n) {
+    while (true) {
+        var p = smallestFactor(n)
+        if (p < n){
+            n /= p
+        }else{
+            return n
+        }
+    }
+}
+
+function smallestFactor(n) {
+    if (n <= 1){
+        throw new IllegalArgumentException();
+    }
+    for (var i = 2, end = Math.sqrt(n); i <= end; i++) {
+        if (n % i == 0){
+            return i
+        }
+    }
+    return n
+}
+
+return run(600851475143);
---
+++ euler_004.js
@@ -0,0 +1,34 @@
+// Problem 4
+// =========
+// 
+// A palindromic number reads the same both ways. The largest palindrome made
+// from the product of two 2-digit numbers is 9009 = 91 * 99.
+// 
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+function run() {
+	var max_pal = -1
+    for (var x = 1000; x > 0; x--) {
+        for (var y = 1000; y > 0; y--) {
+        	var prod = x*y
+            if (is_palindrome(prod) && (prod > max_pal)) {
+            	max_pal = prod
+            	console.log(prod, x, y)
+            }
+        }
+    }
+    return max_pal
+}
+
+function is_palindrome(n) {
+    var n_string = String(n)
+    return n_string === reverse(n_string)
+}
+
+function reverse(s) {
+  for (var i = s.length - 1, o = ''; i >= 0; o += s[i--]) { }
+  return o;
+}
+
+// TODO: return your answer for this prompt.
+return run();
