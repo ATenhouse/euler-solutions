c90783c4fb8b072c34bd80e0846620e28128c987
chistery <Adam Tenhouse, Esquire>
2016-08-14 12:18:12 -0500

    Added a solution scraped from online. Hoping to improve upon it.

---
+++ euler_015.js
@@ -0,0 +1,12 @@
+// Problem 15
+// ==========
+// 
+// Starting in the top left corner of a 2 * 2 grid, there are 6 routes
+// (without backtracking) to the bottom right corner.
+// 
+// How many routes are there through a 20 * 20 grid?
+
+
+
+// TODO: return your answer for this prompt.
+return /* solution */;
