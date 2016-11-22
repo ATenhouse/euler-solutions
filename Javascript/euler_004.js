152b3f34f64d7abe883fb157d1ed7a8ec8250b2e
Adam Tenhouse <merthedil@gmail.com>
2016-11-22 16:56:44 -0600

    minor changes, changed strict equal to more truthy/falsy

--- Javascript/euler_004.js
+++ Javascript/euler_004.js
@@ -7,13 +7,12 @@
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
 function run() {
-	var max_pal = -1
+    var max_pal = -1
     for (var x = 1000; x > 0; x--) {
         for (var y = 1000; y > 0; y--) {
-        	var prod = x*y
+            var prod = x * y
             if (is_palindrome(prod) && (prod > max_pal)) {
-            	max_pal = prod
-            	console.log(prod, x, y)
+                max_pal = prod
             }
         }
     }
@@ -21,13 +20,13 @@ function run() {
 }
 
 function is_palindrome(n) {
-    var n_string = String(n)
-    return n_string === reverse(n_string)
+    n = String(n)
+    return n == reverse(n)
 }
 
 function reverse(s) {
-  for (var i = s.length - 1, o = ''; i >= 0; o += s[i--]) { }
-  return o;
+    for (var i = s.length - 1, o = ''; i >= 0; o += s[i--]) {}
+    return o;
 }
 
 // TODO: return your answer for this prompt.
