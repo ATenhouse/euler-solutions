60b8b03b1bd846cdbc3d087afeb3af5f9609b89f
Adam Tenhouse <merthedil@gmail.com>
2016-11-22 17:30:16 -0600

    Removed console log, etc

--- Javascript/euler_016.js
+++ Javascript/euler_016.js
@@ -4,11 +4,13 @@
 // 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 //
 // What is the sum of the digits of the number 2^1000?
+
 var bigInt = require("big-integer")
 var big_n = bigInt(2).pow(1000).toString()
 var sum = 0
 for (var i = 0; i < big_n.length; i++) {
     sum += Number(big_n[i]);
 }
+
 // TODO: return your answer for this prompt.
 return sum;
