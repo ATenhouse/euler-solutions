b67b5332f0e13f3cea88d37520ef5cd6cbf0bf72
Adam Tenhouse <merthedil@gmail.com>
2016-11-22 17:11:45 -0600

    REALLY hate the fact it's n-cubed, but ...

--- Javascript/euler_009.js
+++ Javascript/euler_009.js
@@ -12,15 +12,15 @@
 
 var result = -1
 for (var i = 1; i <= 497; i++) {
-	for (var j = i; j <= 498; j++) {
-		for (var k = j; k <= 499; k++) {
-			var sum = i + j + k
-			var truth = (Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(k,2))
-			if(sum === 1000 && truth) {
-				result = i * j * k
-			}
-		}
-	}
+    for (var j = i; j <= 498; j++) {
+        for (var k = j; k <= 499; k++) {
+            var sum = i + j + k
+            var truth = (Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(k, 2))
+            if (sum === 1000 && truth) {
+                result = i * j * k
+            }
+        }
+    }
 }
 
 // TODO: return your answer for this prompt.
