aad52f3fb5586dcc6464b7dbbf8fb173bb2c0df7
chistery <Adam Tenhouse, Esquire>
2016-11-22 23:00:11 -0600

    Go figure a recursive implementation of factorials would be SUPER slow.

---
+++ Javascript/euler_034.js
@@ -0,0 +1,35 @@
+// Problem 34
+// ==========
+// 
+// 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+// 
+// Find the sum of all numbers which are equal to the sum of the factorial of
+// their digits.
+// 
+// Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
+function factorial(n) {
+    var out = 1;
+    for (var i = n; i >= 2; i--) {
+        out *= i
+    }
+    return out
+}
+
+function get(x) {
+    return String(x).split("").map(factorial).reduce((a, b) => a + b);
+}
+
+function main(){
+	var sum = 0;
+	for (var i = 50000; i >= 3; i--) {
+		var y = get(i);
+		if(i == y){
+			sum += y;
+		}
+	}
+	return sum;
+}
+
+// TODO: return your answer for this prompt.
+return main();
