0a2cbd31ed4c5cc5eb39cd5e7aa308175435833c
Adam Tenhouse <merthedil@gmail.com>
2016-11-23 09:39:33 -0600

    A little brute force, but it's down to 249ms

---
+++ Javascript/euler_037.js
@@ -0,0 +1,67 @@
+// Problem 37
+// ==========
+// 
+// The number 3797 has an interesting property. Being prime itself, it is
+// possible to continuously remove digits from left to right, and remain
+// prime at each stage: 3797, 797, 97, and 7. Similarly we can work from
+// right to left: 3797, 379, 37, and 3.
+// 
+// Find the sum of the only eleven primes that are both truncatable from left
+// to right and right to left.
+// 
+// NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+
+function main() {
+    var out = []
+    for (var i = 1000000; i >= 8; i--) {
+        if (bigCheck(i)) {
+            out.push(i)
+        }
+    }
+    return out.reduce((a,b) => a + b)
+}
+
+function bigCheck(s) {
+    s = String(s);
+    return checkLeft(s) && checkRight(s);
+}
+
+function checkLeft(s) {
+    while (s.length >= 1) {
+        if (isPrime(s) == false) {
+            return false;
+        }
+        s = s.slice(1);
+    }
+    return true;
+}
+
+function checkRight(s) {
+    while (s.length >= 1) {
+        if (isPrime(s) == false) {
+            return false
+        }
+        s = s.substring(0, s.length - 1);
+    }
+    return true;
+}
+
+function isPrime(n) {
+    n = Number(n)
+    if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
+    if (n % 2 == 0)
+        return (n == 2)
+    if (n % 3 == 0)
+        return (n == 3)
+    var m = Math.sqrt(n);
+    for (var i = 5; i <= m; i += 6) {
+        if (n % i == 0)
+            return false
+        else if (n % (i + 2) == 0)
+            return false
+    }
+    return true
+}
+
+// TODO: return your answer for this prompt.
+return main();
