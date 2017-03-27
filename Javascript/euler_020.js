d0d945153964f6eb9406140e4121694b1cf20420
chistery <Adam Tenhouse, Esquire>
2017-03-26 21:23:42 -0500

    Changed 20 to use the bigInteger in both loops, because why not?

--- Javascript/euler_020.js
+++ Javascript/euler_020.js
@@ -5,15 +5,17 @@
 // 
 // Find the sum of the digits in the number 100!
 
-var bigInt = require("big-integer")
-var full_int = bigInt(1)
-var out_int = 0
+var bigInt = require("big-integer");
+var full_int = bigInt(1);
 for (var i = 100; i >= 1; i--) {
-	full_int = full_int.times(i)
+    full_int = full_int.times(i);
 }
-var s = full_int.toString()
+
+var s = full_int.toString();
+var out_int = bigInt(0);
 for (var i = s.length - 1; i >= 0; i--) {
-	out_int += Number(s[i])
+    out_int = out_int.add(s[i]);
 }
+
 // TODO: return your answer for this prompt.
-return out_int;
+return out_int.toString();
