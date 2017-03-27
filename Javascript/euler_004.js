5b737dea8211381bf556bd7029db2ff4225c2b8d
chistery <Adam Tenhouse, Esquire>
2017-03-26 19:30:38 -0500

    Changed 004 to suit JSHint, also changed is_palindrome to a const

--- Javascript/euler_004.js
+++ Javascript/euler_004.js
@@ -6,22 +6,20 @@
 // 
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-function run() {
-    var max_pal = -1
+function run(digits) {
+    var max_pal = -1;
     for (var x = 1000; x > 0; x--) {
         for (var y = 1000; y > 0; y--) {
-            var prod = x * y
-            if (is_palindrome(prod) && (prod > max_pal)) {
-                max_pal = prod
+            var prod = x * y;
+            if (is_palindrome(prod)) {
+                max_pal = Math.max(max_pal, prod);
             }
         }
     }
-    return max_pal
+    return max_pal;
 }
 
-function is_palindrome(n) {
-    return n == reverse(String(n))
-}
+const is_palindrome = n => n == reverse(String(n));
 
 function reverse(s) {
     for (var i = s.length - 1, o = ''; i >= 0; o += s[i--]) {}
