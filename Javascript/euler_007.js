18ebc81391d391622f0b619f78faa6bbfc206ca9
chistery <Adam Tenhouse, Esquire>
2017-03-26 20:11:21 -0500

    Just housekeeping

--- Javascript/euler_007.js
+++ Javascript/euler_007.js
@@ -9,28 +9,32 @@
 function find_prime(n) {
     for (var i = 2, count = 0;; i++) {
         if (isPrime(i)) {
-            count++
-            if (count == n)
-                return i
+            count++;
+            if (count == n) {
+                return i;
+            }
         }
     }
 }
 
 function isPrime(n) {
     if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
-    if (n % 2 == 0)
-        return (n == 2)
-    if (n % 3 == 0)
-        return (n == 3)
+    if (n % 2 == 0) {
+        return (n == 2);
+    }
+    if (n % 3 == 0) {
+        return (n == 3);
+    }
     var m = Math.sqrt(n);
     for (var i = 5; i <= m; i += 6) {
-        if (n % i == 0)
-            return false
-        else if (n % (i + 2) == 0)
-            return false
+        if (n % i == 0) {
+            return false;
+        } else if (n % (i + 2) == 0) {
+            return false;
+        }
     }
-    return true
+    return true;
 }
 
 // TODO: return your answer for this prompt.
-return find_prime(10001)
+return find_prime(10001);
