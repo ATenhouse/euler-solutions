cd3ed640d281761bd5592183e4e4a7aed18fde14
chistery <Adam Tenhouse, Esquire>
2017-04-09 16:37:57 -0500

    Added simple solution to 63; now passing.

---
+++ Javascript/euler_063.js
@@ -0,0 +1,15 @@
+// Problem 63
+// ==========
+//
+// The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the
+// 9-digit number, 134217728=8^9, is a ninth power.
+//
+// How many n-digit positive integers exist which are also an nth power?
+
+var s = 0;
+for (var n = 1; n < 10; n++) {
+    s += Math.floor(1 / (1 - Math.log10(n)));
+}
+
+// TODO: return your answer for this prompt.
+return s;
