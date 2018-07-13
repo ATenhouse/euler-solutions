569b32123e9df1919a67119beedc24a88478b3f7
Adam Tenhouse <merthedil@gmail.com>
2018-07-12 21:38:25 -0500

    Breaking up is hard to do
    Broke solution for #14 into a separate function for collatz length.

--- Javascript/euler_014.js
+++ Javascript/euler_014.js
@@ -1,44 +1,48 @@
 // Problem 14
 // ==========
-// 
+//
 // The following iterative sequence is defined for the set of positive
 // integers:
-// 
+//
 // n->n/2 (n is even)
 // n->3n+1 (n is odd)
-// 
+//
 // Using the rule above and starting with 13, we generate the following
 // sequence:
 //                   13->40->20->10->5->16->8->4->2->1
-// 
+//
 // It can be seen that this sequence (starting at 13 and finishing at 1)
 // contains 10 terms. Although it has not been proved yet (Collatz Problem),
 // it is thought that all starting numbers finish at 1.
-// 
+//
 // Which starting number, under one million, produces the longest chain?
-// 
+//
 // NOTE: Once the chain starts the terms are allowed to go above one million.
 
-var max_length = 1,
-    largest_start = 0;
-for (var i = 999999; i > 0; i--) {
-    var inner_length = 1,
-        n = i
-    while (n > 1) {
-        if (n % 2 == 0) {
-            n /= 2
-            inner_length += 1
+function sequence(n) {
+    var count = 0;
+    while (n !== 1) {
+        if (n % 2 === 0) {
+            n = n / 2;
         } else {
-            n = (3 * n) + 1
-            inner_length += 1
+            n = (3 * n) + 1;
         }
+        count++;
     }
+    return count;
+}
+
+var max_length = 0;
+var largest_start = 0;
+var i = 1000000;
+while (i) {
+    var inner_length = sequence(i);
     if (inner_length > max_length) {
-        max_length = inner_length
-        largest_start = i
+        max_length = inner_length;
+        largest_start = i;
     }
+    i--;
 }
 
 // TODO: return your answer for this prompt.
-// console.log(largest_start)
 return largest_start;
